a9ffdd089181f849295b32252fe41e20d98b4108
3A532055 <ylhc214044@gmail.com>
2019-01-12 20:58:26 +0800

    建立postcomment路由&使用者頭像路由

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,12 @@ Route::get('search', 'HomeController@search');
 
 Route::resource('post', 'PostController');
 Route::resource('type', 'PostTypeController', ['except' => ['index']]);
+Route::resource('post.comment', 'PostCommentController', ['only' => ['store', 'destroy']]);
 
 Route::auth();
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
+Route::group(['prefix'=>'user','middleware'=>['auth']],function(){
+    Route::get('avatar','UserController@getAvatar');
+    Route::post('avatar','UserController@postAvatar');
+});
