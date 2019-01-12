a48022100263eff74dd9a8fe77abc2c5f58e573c
3A532055 <ylhc214044@gmail.com>
2019-01-12 16:30:32 +0800

    路由規劃

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('search', 'HomeController@search');
+
+Route::get('login','Auth\AuthController@showLoginForm');
+Route::post('login','Auth\AuthController@login');
+Route::get('logout','Auth\AuthController@logout');
+
+Route::resource('post', 'PostController');
+Route::resource('type', 'PostTypeController', ['except' => ['index']]);
