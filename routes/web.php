8c97c50aa5d627f401ed11750086a89e8b14a46e
3A532055 <ylhc214044@gmail.com>
2019-01-12 20:15:47 +0800

    修正

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@
 Route::get('/', 'HomeController@index');
 Route::get('search', 'HomeController@search');
 
-//Route::get('login','Auth\AuthController@showLoginForm');
-//Route::post('login','Auth\AuthController@login');
-//Route::get('logout','Auth\AuthController@logout');
-
 Route::resource('post', 'PostController');
 Route::resource('type', 'PostTypeController', ['except' => ['index']]);
 
