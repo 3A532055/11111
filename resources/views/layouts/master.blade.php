b3b309b982e9e95fd95ee0befb48fdda1555faed
3A532055 <ylhc214044@gmail.com>
2019-01-12 16:47:41 +0800

    建立主樣板

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>勤益米其林評鑑網  @yield('title')</title>
+    @include('partials.head')
+</head>
+<body>
+@include('partials.nav')
+<div class="container">
+    @section('content')
+    @show
+</div>
+</body>
+</html>
