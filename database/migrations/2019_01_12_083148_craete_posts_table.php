2748da5b51a3bacd8f39316d92c7e2859089f486
3A532055 <ylhc214044@gmail.com>
2019-01-12 16:34:34 +0800

    migration:posts&posts_type

---
+++ database/migrations/2019_01_12_083148_craete_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CraetePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->unsignedInteger('type')->nullable();
+            $table->text('content')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
